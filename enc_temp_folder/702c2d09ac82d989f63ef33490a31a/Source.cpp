4ec4b9ce9f02ccc28c633388850fdd347ac0cc82
bminnz <bminn@DESKTOP-GL1GAKT>
2021-11-07 21:19:31 -0500

    Don't forget you need parentheses to add up all the numbers before dividing. the f in 3.0f isn't necessary. Also noticed line 16 was n2 instead of n3. Make sure to try to prompt the user to enter something. Don't just expect them to enter inputs.

--- enc_temp_folder/702c2d09ac82d989f63ef33490a31a/Source.cpp
+++ enc_temp_folder/702c2d09ac82d989f63ef33490a31a/Source.cpp
@@ -11,9 +11,9 @@ int main()
 {
 	int n1 =0 , n2 = 0, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
-	cout << "Enter First number: "; cin >> n1;
-	cout << "Enter Second number: "; cin >> n2;
-	cout << "Enter Third number: "; cin >> n3;
+	cout << "First number: "; cin >> n1;
+	cout << "Second number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
